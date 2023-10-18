db5bca6ca52e1d4279f3f28f181be38803e59af3
rizelliot <filipkozuchowski@gmail.com>
2023-10-18 10:41:24 +0200

    complete another challenge

---
+++ 7kyu/coding_meetup.js
@@ -0,0 +1,28 @@
+// You will be given an array of objects (hashes in ruby) representing data about developers who have signed up to attend the coding meetup that you are organising for the first time.
+
+// Your task is to return the number of JavaScript developers coming from Europe.
+
+// For example, given the following list:
+
+// var list1 = [
+//   { firstName: 'Noah', lastName: 'M.', country: 'Switzerland', continent: 'Europe', age: 19, language: 'JavaScript' },
+//   { firstName: 'Maia', lastName: 'S.', country: 'Tahiti', continent: 'Oceania', age: 28, language: 'JavaScript' },
+//   { firstName: 'Shufen', lastName: 'L.', country: 'Taiwan', continent: 'Asia', age: 35, language: 'HTML' },
+//   { firstName: 'Sumayah', lastName: 'M.', country: 'Tajikistan', continent: 'Asia', age: 30, language: 'CSS' }
+// ];
+
+// your function should return number 1.
+
+// If, there are no JavaScript developers from Europe then your function should return 0.
+
+// My solution
+
+function countDevelopers(list) {
+    let counter = 0;
+    list.forEach(developer => {
+      if (developer.continent === 'Europe' && developer.language != 'HTML') {
+        counter++
+      }
+    })
+    return counter;
+  }
