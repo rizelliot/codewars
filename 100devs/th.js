7812e154f59e9afe554427d1a353d47f7d631840
rizelliot <filipkozuchowski@gmail.com>
2023-10-27 17:13:19 +0200

    initial commit

---
+++ 100devs/th.js
@@ -0,0 +1,9 @@
+function Skater(energy, agility, speed) {
+    this.energy = energy
+    this.agility = agility
+    this.speed = speed
+
+   this.doATrick = function() {
+    console.log('dosomething')
+   }
+}
