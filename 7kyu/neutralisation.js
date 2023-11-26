690eb9c596928ddcb8595a7cea4be897660212a5
rizelliot <filipkozuchowski@gmail.com>
2023-11-26 15:29:12 +0100

    complete chall

---
+++ 7kyu/neutralisation.js
@@ -0,0 +1,43 @@
+// Given two strings comprised of + and -, return a new string which shows how the two strings interact in the following way:
+
+//     When positives and positives interact, they remain positive.
+//     When negatives and negatives interact, they remain negative.
+//     But when negatives and positives interact, they become neutral, and are shown as the number 0.
+
+// Worked Example
+
+// ("+-+", "+--") ➞ "+-0"
+// # Compare the first characters of each string, then the next in turn.
+// # "+" against a "+" returns another "+".
+// # "-" against a "-" returns another "-".
+// # "+" against a "-" returns "0".
+// # Return the string of characters.
+
+// Examples
+
+// ("--++--", "++--++") ➞ "000000"
+
+// ("-+-+-+", "-+-+-+") ➞ "-+-+-+"
+
+// ("-++-", "-+-+") ➞ "-+00"
+
+// Notes
+
+// The two strings will be the same length.
+
+
+// My solution:
+
+function neutralise(s1, s2) {
+  let resultString = "";
+
+  for (i = 0; i < s1.length; i++) {
+    if (s1[i] != s2[i]) {
+      resultString += 0;
+    } else {
+      resultString += s1[i];
+    }
+  }
+
+  return resultString;
+}
