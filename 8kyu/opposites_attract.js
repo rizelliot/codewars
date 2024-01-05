e2f2afc1319029cd0852018d42e14a5211106f37
rizelliot <filipkozuchowski@gmail.com>
2024-01-05 12:21:18 +0100

    change type operator in the challenge

--- 8kyu/opposites_attract.js
+++ 8kyu/opposites_attract.js
@@ -6,7 +6,7 @@
 // My solution:
 
 function lovefunc(flower1, flower2){
-    if (flower1 % 2 === 0 && flower2 % 2 != 0 || flower2 % 2 === 0 && flower1 % 2 != 0) {
+    if (flower1 % 2 === 0 && flower2 % 2 !== 0 || flower2 % 2 === 0 && flower1 % 2 !== 0) {
       return true
     } return false
   }
